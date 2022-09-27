ea889fc236d9a14f909307c021a8b28bcb3cfa77
hhayies <107159329+hhayies@users.noreply.github.com>
2022-09-27 14:19:44 +0000

    ddd, Sep 9/27/2022, YYYY, 2:19 A +0

--- combined/static/js/script.js
+++ combined/static/js/script.js
@@ -142,8 +142,8 @@ $(function() {
     socket.on('good_countup', function (msg) {
         $('#good_count').html(msg.good_count);
         // setTimeoutを解除
-        if (msg.good_count == 1) {
-            
+        if (msg.good_count != 1) {
+
         }
         //　リセット時間の設定
         //　setTimeout更新
